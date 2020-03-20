a9f18dddee11f8e49d75be471811686eed987592
masvgp <masvgp@users.noreply.github.com>
2020-03-19 20:11:27 -0600

    Created Date model and DateForm model form. Created get_date_info view function. Created date_info.html to display DateForm forms. Created url path from exam_info.html to date_info.html.

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'exam_info', views.get_exam_info)
+    url(r'exam_info', views.get_exam_info),
+    url(r'date_info', views.get_date_info),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Exam, ExamForm
+from .models import Date, DateForm
+import datetime
 
 
 # Create your views here.
@@ -10,25 +12,41 @@ def home(request):
 
 def get_exam_info(request):
     '''View function gets exam info data from exam_info.html and pases it to ExamForm which passes the data to the Exam model which stores the data in the data base'''
+
+    # The following block will slice the scheduled exams by a given date and then
+    # count the number of students scheduled on that date. I should probably create
+    # a database to store the total number of exam scheduled by day. This way, we can just
+    # query this data base to set dates.
+    # exam_query = Exam.objects.all()
+    # exams_by_date = exam_query.filter(start_date=datetime.date(2020, 3, 31))
+    # num_students_scheduled = 0
+    # for exam in exams_by_date:
+    #     num_students_scheduled += exam.num_students
+    # print(num_students_scheduled)
+    # print(Exam.objects.filter(start_date__lte='2020-03-31'))
+
     if request.method == 'POST':
         exam_form = ExamForm(request.POST)
         if exam_form.is_valid():
             exam_form.save()
-            return HttpResponseRedirect('/')
+            ''' Put function here that passes data from "exam_form" to confirmation page '''
+            date_form = DateForm()
+            return render(request, 'date_info.html', {'date_form': date_form})
     else:
         exam_form = ExamForm()
 
     return render(request, 'exam_info.html', {'exam_form': exam_form})
 
 
-# def exam_info(request):
-#     if request.method == "Post":
-#         # test_request is for pulling user creds from the admin - use later after admin is set up
-#         # test_request = Invitation(from_user=request.user)
+def get_date_info(request):
+    if request.method == 'POST':
+        date_form = DateForm(request.POST)
+        if date_form.is_valid():
+            date_form.save()
 
-#         form = exam_info_form(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('welcome')
-#     else:
-#         return render(request, "exam_info.html")
+            return HttpResponseRedirect('/')
+
+    else:
+        date_form = DateForm()
+
+    return render(request, 'date_info.html', {'date_form': date_form})
