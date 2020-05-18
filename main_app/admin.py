f961bb82693d64bbf3cbe4300d6c9b7ae5ce37e2
McKinnin <lloydmckinnin@gmail.com>
2020-05-18 11:47:52 -0600

    Added some views to the admin page. Now trying to get it to move tests depending on there status

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from main_app.models import Exam, Date
+from main_app.models import Exam, Date, AcceptedExams
 
-admin.site.register(Exam)
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('course_name', 'num_students', 'instructor_last_name', 'test_accepted')
+    list_editable = ('test_accepted',)
+
 admin.site.register(Date)
+admin.site.register(AcceptedExams)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from django.forms import ModelForm
 from django import forms
@@ -27,11 +29,38 @@ class Exam(models.Model):
     comment = models.CharField(max_length=300, blank=True)
     test_accepted = models.BooleanField(default=False)
 
+    def __str__(self):
+        for i in Exam.objects.raw('Select * from main_app_exam where test_accepted == True'):
+            AcceptedExams.objects.raw('INSERT INTO main_app_acceptedexams(instructor_first_name, instructor_last_name, department, course_name, course_number, section_number, num_students, calculator, notes, computer_exam, scantron, timed, dictionary, comment, test_accepted) VALUES (' + i.instructor_first_name + 
+            ',' + i.instructor_last_name + ',' + i.department + ',' + i.course_name + ',' + i.course_number + ',' + 
+            i.section_number + ',' + i.num_students + ',' + i.calculator + ',' + i.notes + ',' + i.computer_exam + ',' +
+            i.scantron + ',' + i.timed + ',' + i.dictionary + ',' + i.comment + ',' + i.test_accepted + ')')
+        return "{0}".format(
+            self.course_name)
+
+
+class AcceptedExams(models.Model):
+    instructor_first_name = models.CharField(max_length=30)
+    instructor_last_name = models.CharField(max_length=30)
+    department = models.CharField(max_length=30, null=True)
+    course_name = models.CharField(max_length=30, null=True)
+    course_number = models.IntegerField(null=True)
+    section_number = models.IntegerField(null=True)
+    num_students = models.IntegerField(null=True)
+    calculator = models.CharField(max_length=30, blank=True)
+    notes = models.CharField(max_length=30, blank=True)
+    computer_exam = models.BooleanField(default=False)
+    scantron = models.BooleanField(default=False)
+    timed = models.BooleanField(default=False)
+    dictionary = models.BooleanField(default=False)
+    comment = models.CharField(max_length=300, blank=True)
+    test_accepted = models.BooleanField(default=False)
 
 class Date(models.Model):
     exam = models.OneToOneField(Exam,
                                 on_delete=models.CASCADE,
-                                primary_key=True
+                                primary_key=True,
+                                default=True
                                 )
     start_date = models.DateField()
     end_date = models.DateField()
